35f831ea33c0954d93bb542ca57bf09afb533d82
Sam <17832562+swoolf@users.noreply.github.com>
2023-04-26 14:51:59 -0400

    First!

---
+++ README.md
@@ -0,0 +1,13 @@
+#LVN Conversation Recorder Demo
+Record a conversation from your browser, and upload it to LVN.
+
+This benefits from the following open source libraries
+
+Simple WebAudioRecorder.js demo
+A simple HTML/JS demo that uses [WebAudioRecorder.js](https://github.com/higuma/web-audio-recorder-js) to record mp3, wav and Vorbis audio on a web page.
+
+tarballjs
+https://github.com/ankitrohatgi/tarballjs
+
+Pagecrypt
+https://www.maxlaumeister.com/pagecrypt/
---
+++ js/app.js
@@ -0,0 +1,215 @@
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
+
+var gumStream; 						//stream from getUserMedia()
+var recorder; 						//WebAudioRecorder object
+var input; 							//MediaStreamAudioSourceNode  we'll be recording
+var encodingType; 					//holds selected encoding for resulting audio (file)
+var encodeAfterRecord = true;       // when to encode
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext; //new audio context to help us record
+
+var encodingTypeSelect = document.getElementById("encodingTypeSelect");
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+
+
+var lvnJSON ={
+			"schema_version": "1.0",
+			"source_type": "hearth/2.0",
+			"source_id": "R&D Upload Page",
+			"host_id": 161,
+			"host_name": "Guest",
+			"geo": [],
+			"cross_pollination": [],
+			"start_time": "2023-04-20T16:22:50",
+			"end_time": "2023-04-20T16:24:14",
+			"duration": 83,
+			"collection_id": 243,
+			"environment": "production",
+			"language_codes": [
+			"en-US"
+			],
+			"title": "R&D Upload Test",
+			"num_participants": 2
+		}
+
+function startRecording() {
+	console.log("startRecording() is called");
+	console.log("jere");
+
+	/*
+		Simple constraints object, for more advanced features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
+    
+    var constraints = { audio: true, video:false }
+
+    /*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		__log("getUserMedia() success, stream created, initializing WebAudioRecorder...");
+
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
+
+		*/
+		audioContext = new AudioContext();
+
+		//update the format 
+		document.getElementById("formats").innerHTML="Format: 2 channel "+encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value+" @ "+audioContext.sampleRate/1000+"kHz"
+
+		//assign to gumStream for later use
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+		
+		//stop the input from playing back through the speakers
+		//input.connect(audioContext.destination)
+
+		//get the encoding 
+		encodingType = encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value;
+		
+		//disable the encoding selector
+		encodingTypeSelect.disabled = true;
+
+		recorder = new WebAudioRecorder(input, {
+		  workerDir: "js/", // must end with slash
+		  encoding: encodingType,
+		  numChannels:2, //2 is the default, mp3 encoding supports only 2
+		  onEncoderLoading: function(recorder, encoding) {
+		    // show "loading encoder..." display
+		    __log("Loading "+encoding+" encoder...");
+		  },
+		  onEncoderLoaded: function(recorder, encoding) {
+		    // hide "loading encoder..." display
+		    __log(encoding+" encoder loaded");
+		  }
+		});
+
+		recorder.onComplete = function(recorder, blob) { 
+			__log("Encoding complete");
+			sendToLVN(blob);
+			createDownloadLink(blob,recorder.encoding);
+			encodingTypeSelect.disabled = false;
+		}
+
+		recorder.setOptions({
+		  timeLimit:120,
+		  encodeAfterRecord:encodeAfterRecord,
+	      ogg: {quality: 0.5},
+	      mp3: {bitRate: 160}
+	    });
+
+		//start the recording process
+		recorder.startRecording();
+
+		 __log("Recording started");
+
+	}).catch(function(err) {
+	  	//enable the record button if getUSerMedia() fails
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+
+	});
+
+	//disable the record button
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function stopRecording() {
+	console.log("stopRecording() called");
+	
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	//disable the stop button
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+	
+	//tell the recorder to finish the recording (stop recording + encode the recorded audio)
+	recorder.finishRecording();
+
+	__log('Recording is stopped');
+}
+
+function createDownloadLink(blob,encoding) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+	//link the a element to the blob
+	link.href = url;
+	link.download = new Date().toISOString() + '.'+encoding;
+	link.innerHTML = link.download;
+
+	//add the new audio and a elements to the li element
+	li.appendChild(au);
+	li.appendChild(link);
+
+	//add the li element to the ordered list
+	// recordingsList.appendChild(li);
+}
+
+
+
+//helper function
+function __log(e, data) {
+	log.innerHTML += "\n" + e + " " + (data || '');
+}
+
+async function sendToLVN(audioblob){
+			__log("send to LVN");
+			var myHeaders = new Headers();
+			myHeaders.append("Content-Type", "application/x-tar");
+
+			lvnJSON.title = document.getElementById('convoname').value;
+
+			const str = JSON.stringify(lvnJSON);
+			const bytes = new TextEncoder().encode(str);
+			const jsonblob = new Blob([bytes], {
+				type: "application/json;charset=utf-8"
+			});
+			// let jsonblob = await Blob([lvnJSON], {type: "application/json"})
+
+			// let prejson = await fetch('http://127.0.0.1:8000/audio.json');
+			// let json = await prejson.blob();
+
+			let tar = new tarball.TarWriter();
+			tar.addFile("20230420-162252.mp3", audioblob);
+			tar.addFile("20230420-162252.mp3.json", jsonblob);
+
+			let data = await tar.makeBlob();
+
+			var requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: data,
+			redirect: 'follow'
+			};
+
+			fetch(LVeNdpoint, requestOptions)
+			.then(response => console.log(response.text()))
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));
+			__log("successfully uploaded to LVN");
+}
